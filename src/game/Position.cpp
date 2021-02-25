c15dabd27a0096aae774dc550959dfb80d75f348
julian-fuchs <julian7fuchs@gmail.com>
2021-02-25 13:09:03 +0100

    Add support for operators

--- src/game/Position.cpp
+++ src/game/Position.cpp
@@ -4,3 +4,39 @@ Position::Position(int x, int y) {
   this->x = x;
   this->y = y;
 }
+
+Position Position::operator+(const Position &other_position) const {
+  return Position(x + other_position.x, y + other_position.y);
+}
+
+Position Position::operator-(const Position &other_position) const {
+  return Position(x - other_position.x, y - other_position.y);
+}
+
+Position Position::operator%(const Position &other_position) const {
+  return Position(x % other_position.x, y % other_position.y);
+}
+
+bool Position::operator==(const Position &other_position) const{
+  return (x == other_position.x && y == other_position.y);
+}
+
+bool Position::operator!=(const Position &other_position) const{
+  return (x != other_position.x || y != other_position.y);
+}
+
+bool Position::operator<(const Position &other_position) const{
+  return (*this != other_position && x <= other_position.x && y <= other_position.y);
+}
+
+bool Position::operator<=(const Position &other_position) const{
+  return (*this < other_position || *this == other_position);
+}
+
+bool Position::operator>(const Position &other_position) const{
+  return (*this != other_position && x >= other_position.x && y >= other_position.y);
+}
+
+bool Position::operator>=(const Position &other_position) const{
+  return (*this > other_position || *this == other_position);
+}
--- src/game/Position.h
+++ src/game/Position.h
@@ -9,6 +9,18 @@ class Position {
   distance_t y;
 
   Position(int x, int y);
+
+  Position operator+(const Position& other_position) const;
+  Position operator-(const Position& other_position) const;
+  Position operator%(const Position& other_position) const;
+
+  bool operator==(const Position& other_position) const;
+  bool operator!=(const Position& other_position) const;
+  bool operator<(const Position& other_position) const;
+  bool operator<=(const Position& other_position) const;
+  bool operator>(const Position& other_position) const;
+  bool operator>=(const Position& other_position) const;
 };
 
+typedef Position object_size_t;
 #endif  // CROSSING_CONSOLES_POSITION_H
