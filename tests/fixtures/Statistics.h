04a44d93b286c26140256ca01fc71422ec218e95
AnneRei <anne.reichert97@gmail.com>
2021-03-29 18:15:25 +0200

    Extend statistic testing

--- tests/fixtures/Statistics.h
+++ tests/fixtures/Statistics.h
@@ -1,8 +1,27 @@
 #ifndef CROSSING_CONSOLES_STATISTICS_H
 #define CROSSING_CONSOLES_STATISTICS_H
 
-#include "ConnectionManagers.h"
+#include <gtest/gtest.h>
 
-class Statistics : public ConnectionManagers {};
+#include "../../src/communication/byte_layer/byte_stream/MockBidirectionalByteStream.h"
+#include "Connections.h"
+
+class Statistics : public Connections {
+ public:
+  void establish_connections() {
+    auto stream_pair = MockBidirectionalByteStream::CreatePair();
+    auto &server_side = stream_pair.first;
+    auto &client_side = stream_pair.second;
+
+    auto client_message_input_stream = std::make_shared<MessageInputStream>(client_side);
+    auto server_message_output_stream = std::make_shared<MessageOutputStream>(server_side);
+
+    auto server_message_input_stream = std::make_shared<MessageInputStream>(server_side);
+    auto client_message_output_stream = std::make_shared<MessageOutputStream>(client_side);
+
+    make_connections(client_message_input_stream, client_message_output_stream, server_message_input_stream,
+                     server_message_output_stream);
+  };
+};
 
 #endif  // CROSSING_CONSOLES_STATISTICS_H
--- tests/statistics.cpp
+++ tests/statistics.cpp
@@ -2,58 +2,146 @@
 
 #include <gtest/gtest.h>
 
-#include "fixtures/ConnectionManagers.h"
+#include "fixtures/Connections.h"
 
 using namespace communication;
 using namespace communication::byte_layer;
 using namespace communication::connection_layer;
 using namespace communication::message_layer;
 
-TEST_F(Statistics, NoMessagesStatistics) {
+void check_statistics_balanced_by_type(const ConnectionStatistics& statistic_first,
+                                       const ConnectionStatistics& statistic_second) {
+  auto statistic_map_received_first = statistic_first.GetReceivedMessageStatistics().count_by_type;
+  auto statistic_map_sent_first = statistic_first.GetSentMessageStatistics().count_by_type;
+  auto statistic_map_sent_second = statistic_second.GetSentMessageStatistics().count_by_type;
+  auto statistic_map_received_second = statistic_second.GetReceivedMessageStatistics().count_by_type;
+
+  // All messages sent by first connection are received by second connection
+  for (auto& first_it : statistic_map_sent_first) {
+    auto second_it = statistic_map_received_second.find(first_it.first);
+
+    if (second_it != statistic_map_received_second.end()) {
+      ASSERT_EQ(first_it.second, second_it->second);
+    } else {
+      ASSERT_FALSE(true);
+    }
+  }
+
+  // All messages sent by second connection are received by first connection
+  for (auto& second_it : statistic_map_sent_second) {
+    auto first_it = statistic_map_received_first.find(second_it.first);
+
+    if (first_it != statistic_map_received_first.end()) {
+      ASSERT_EQ(second_it.second, first_it->second);
+    } else {
+      ASSERT_FALSE(true);
+    }
+  }
+}
+
+void check_statistics_balanced(const ConnectionStatistics& statistic_first,
+                               const ConnectionStatistics& statistic_second) {
+  // Sent and received messages of server and client should complement each other
+
+  ASSERT_EQ(statistic_first.GetReceivedMessageStatistics().total_count,
+            statistic_second.GetSentMessageStatistics().total_count);
+  ASSERT_EQ(statistic_first.GetSentMessageStatistics().total_count,
+            statistic_second.GetReceivedMessageStatistics().total_count);
+
+  check_statistics_balanced_by_type(statistic_first, statistic_second);
+
+  ASSERT_EQ(statistic_first.CalculatePackageLoss().package_loss, 0);
+  ASSERT_EQ(statistic_first.CalculatePackageLoss().package_loss_percentage, 0);
+
+  ASSERT_EQ(statistic_second.CalculatePackageLoss().package_loss, 0);
+  ASSERT_EQ(statistic_second.CalculatePackageLoss().package_loss_percentage, 0);
+}
+
+TEST_F(Statistics, NoMessages) {
   // no messages have been sent yet, statistics should be empty / zero
-  create_server_and_client();
 
-  ConnectionStatistics client_statistics =
-      client_manager->GetStatisticsFromPartnerConnection(ProtocolDefinition::server_partner_id);
-  ASSERT_EQ(client_statistics.GetSentMessageStatistics().total_count, 0);
-  ASSERT_EQ(client_statistics.GetReceivedMessageStatistics().total_count, 0);
-  ASSERT_FALSE(client_statistics.CalculateAverageResponseTime().has_value());
-  ASSERT_EQ(client_statistics.CalculatePackageLoss().package_loss, 0);
-  ASSERT_EQ(client_statistics.CalculatePackageLoss().package_loss_percentage, 0);
+  establish_connections();
+
+  auto statistics_server = server_connection->GetConnectionStatistics();
+  auto statistics_client = client_connection->GetConnectionStatistics();
+
+  ASSERT_EQ(statistics_server.GetReceivedMessageStatistics().total_count, 0);
+  ASSERT_EQ(statistics_server.GetSentMessageStatistics().total_count, 0);
+  ASSERT_EQ(statistics_server.GetSentAndAcknowledgedMessageStatistics().total_count, 0);
+
+  check_statistics_balanced(statistics_server, statistics_client);
+  ASSERT_EQ(statistics_client.GetSentAndAcknowledgedMessageStatistics().total_count, 0);
 }
 
-TEST_F(Statistics, SendMessagesStatistics) {
-  int message_count = 10;
-  create_server_and_client();
-  send_and_check_messages(message_count);
-  ConnectionStatistics client_statistics =
-      client_manager->GetStatisticsFromPartnerConnection(ProtocolDefinition::server_partner_id);
+TEST_F(Statistics, Simple) {
+  // Send all types of Messages from server to client without any unacknowledged packages
 
-  ConnectionStatistics::message_count_map_t sent_map = client_statistics.GetSentMessageStatistics().count_by_type;
-  ASSERT_EQ(sent_map.at(MessageType::PAYLOAD), message_count);
-  ASSERT_EQ(sent_map.at(MessageType::ACKNOWLEDGE), message_count);
+  establish_connections();
 
-  ConnectionStatistics::message_count_map_t received_map =
-      client_statistics.GetReceivedMessageStatistics().count_by_type;
-  ASSERT_EQ(received_map.at(MessageType::PAYLOAD), message_count);
-  ASSERT_EQ(received_map.at(MessageType::ACKNOWLEDGE), message_count);
+  // clients sends two Messages of Type KeepAlive, server sends two messages of type Payload
+  test_connections();
+  test_connections();
 
-  ASSERT_EQ(client_statistics.CalculatePackageLoss().package_loss, 0);
-  ASSERT_EQ(client_statistics.CalculatePackageLoss().package_loss_percentage, 0);
+  // server sends message of type CloseConnection
+  close_connection(server_connection, client_connection);
+
+  auto statistic_server = server_connection->GetConnectionStatistics();
+  auto statistic_client = client_connection->GetConnectionStatistics();
+
+  ASSERT_TRUE(statistic_server.CalculateAverageResponseTime().has_value());
+  ASSERT_TRUE(statistic_client.CalculateAverageResponseTime().has_value());
+
+  ASSERT_EQ(statistic_client.GetSentAndAcknowledgedMessageStatistics().total_count, 2);
+  ASSERT_EQ(statistic_server.GetSentAndAcknowledgedMessageStatistics().total_count, 3);
+
+  check_statistics_balanced(statistic_server, statistic_client);
 }
 
-TEST_F(Statistics, PackageLossStatistics) {
-  // Server becomes unreachable, package loss is 100%
-  create_server_and_client();
+TEST_F(Statistics, ManyMessages) {
+  // Send 100 Messages from server to client and vice versa
 
-  std::vector<uint8_t> payload;
-  client_manager->SendDataToServer(payload);
+  establish_connections();
 
-  ConnectionStatistics client_statistics =
-      client_manager->GetStatisticsFromPartnerConnection(ProtocolDefinition::server_partner_id);
-  ConnectionStatistics::message_count_map_t sent_map = client_statistics.GetSentMessageStatistics().count_by_type;
+  int count = 100;
+  for (int i = 0; i < count; i++) {
+    // clients sends one message of type KeepAlive, server sends one message of type Payload on each call
+    test_connections();
+  }
 
-  ASSERT_EQ(sent_map.at(MessageType::PAYLOAD), 1);
-  ASSERT_EQ(client_statistics.CalculatePackageLoss().package_loss, 1);
-  ASSERT_EQ(client_statistics.CalculatePackageLoss().package_loss_percentage, 100);
+  // client sends message of type CloseConnection
+  close_connection(client_connection, server_connection);
+
+  auto statistic_server = server_connection->GetConnectionStatistics();
+  auto statistic_client = client_connection->GetConnectionStatistics();
+
+  ASSERT_TRUE(statistic_server.CalculateAverageResponseTime().has_value());
+  ASSERT_TRUE(statistic_client.CalculateAverageResponseTime().has_value());
+
+  // client has one sent & acknowledged message more than server because of connection reset
+  ASSERT_EQ(statistic_client.GetSentAndAcknowledgedMessageStatistics().total_count, count + 1);
+  ASSERT_EQ(statistic_server.GetSentAndAcknowledgedMessageStatistics().total_count, count);
+
+  check_statistics_balanced(statistic_server, statistic_client);
+}
+
+TEST_F(Statistics, Uptime) {
+  // Test increase and range of uptime
+
+  establish_connections();
+
+  auto sleep_time = std::chrono::microseconds(1000);
+  std::this_thread::sleep_for(sleep_time);
+
+  auto uptime_server = server_connection->GetConnectionStatistics().CalculateUptime();
+  auto uptime_client = client_connection->GetConnectionStatistics().CalculateUptime();
+
+  // Check that uptime is in a reasonable range, half of the sleep_time is added as buffer
+  ASSERT_TRUE(uptime_server.count() > 0 && uptime_server < 1.5 * sleep_time);
+  ASSERT_TRUE(uptime_client.count() > 0 && uptime_client < 1.5 * sleep_time);
+
+  std::this_thread::sleep_for(sleep_time);
+
+  // Uptime increases
+  ASSERT_TRUE(uptime_server < server_connection->GetConnectionStatistics().CalculateUptime());
+  ASSERT_TRUE(uptime_client < client_connection->GetConnectionStatistics().CalculateUptime());
 }
