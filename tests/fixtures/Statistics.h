d83cc208141515793ae40bf2c06b8a539eb1e3f9
Martina <martina.scheffler00@gmail.com>
2021-03-29 10:01:38 +0200

    Include ConnectionManagers.h

--- tests/fixtures/Statistics.h
+++ tests/fixtures/Statistics.h
@@ -1,6 +1,8 @@
 #ifndef CROSSING_CONSOLES_STATISTICS_H
 #define CROSSING_CONSOLES_STATISTICS_H
 
+#include "ConnectionManagers.h"
+
 class Statistics : public ConnectionManagers {};
 
 #endif  // CROSSING_CONSOLES_STATISTICS_H
