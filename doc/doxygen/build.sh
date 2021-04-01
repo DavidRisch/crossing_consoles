fa7304a196725bc8e7ffd812978e73b54405b6f5
David Risch <acc_mid@rischnet.de>
2021-04-01 18:58:49 +0200

    Add shell script to build documentation

---
+++ doc/doxygen/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e # exit on error
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+REPO_ROOT="$DIR/../.."
+BUILD_DIR="$REPO_ROOT/cmake-build-doxygen"
+
+mkdir -p "$BUILD_DIR"
+
+cd "$BUILD_DIR"
+
+cmake ..
+
+make doc_doxygen
+
+echo "Doxygen build done"
