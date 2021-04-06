96fe2d92c486acc93190ea5dd67b25a3cd674068
David Risch <acc_mid@rischnet.de>
2021-04-06 23:01:49 +0200

    Add signal handler for ctrl+c to game client main

--- src/main_client.cpp
+++ src/main_client.cpp
@@ -1,3 +1,5 @@
+#include <cassert>
+#include <csignal>
 #include <iostream>
 
 #include "game/GameClient.h"
@@ -9,10 +11,32 @@ using namespace game::common;
 using namespace game::world;
 using namespace game::terminal;
 
+GameClient *GAME_CLIENT;
+
+void SignalHandler(int signal) {
+  assert(signal == SIGINT);
+  assert(GAME_CLIENT != nullptr);
+  GAME_CLIENT->StartExit();
+}
+
+void SetupSignalHandler(GameClient *game_client) {
+  GAME_CLIENT = game_client;
+
+  struct sigaction sigIntHandler {};
+  sigIntHandler.sa_handler = SignalHandler;
+  sigemptyset(&sigIntHandler.sa_mask);
+  sigIntHandler.sa_flags = 0;
+
+  sigaction(SIGINT, &sigIntHandler, nullptr);
+}
+
 int main() {
   std::cout << "main client" << std::endl;
   auto player = std::make_shared<Player>("user", Position(0, 0));
   auto terminal = std::make_shared<RealTerminal>();
   GameClient game_client(player, terminal, coordinate_size_t(1, 1), true);
+
+  SetupSignalHandler(&game_client);
+
   game_client.Run();
 }
