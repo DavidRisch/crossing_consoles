fb3593726d28cef7ad5a9780339773f7cdf28016
julian-fuchs <julian7fuchs@gmail.com>
2021-04-11 18:28:58 +0200

    Change world size

--- src/main_client.cpp
+++ src/main_client.cpp
@@ -13,6 +13,6 @@ int main() {
   std::cout << "main client" << std::endl;
   auto player = std::make_shared<Player>("user", Position(0, 0));
   auto terminal = std::make_shared<RealTerminal>();
-  GameClient game_client(player, terminal, coordinate_size_t(1, 1), true);
+  GameClient game_client(player, terminal, coordinate_size_t(100, 100), true);
   game_client.Run();
 }
