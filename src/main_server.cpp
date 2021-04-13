12d9c3a085a68c2dd9d20b49cd9f18cdd470ecdf
David Risch <acc_mid@rischnet.de>
2021-04-13 20:18:27 +0200

    Increase default size in main_server.cpp

--- src/main_server.cpp
+++ src/main_server.cpp
@@ -6,9 +6,19 @@
 using namespace game;
 using namespace game::common;
 
-int main() {
-  std::cout << "main server" << std::endl;
-  GameServer server(coordinate_size_t(30, 30));
+int main(int argc, char *argv[]) {
+  coordinate_size_t world_size(100, 100);
+
+  if (argc == 3) {
+    world_size.x = std::stoi(argv[1]);
+    assert(world_size.x > 0);
+    world_size.y = std::stoi(argv[2]);
+    assert(world_size.y > 0);
+  }
+
+  std::cout << "Starting server..." << std::endl;
+  GameServer server(world_size);
+  std::cout << "Server ready" << std::endl;
   while (true) {
     server.RunIteration();
     std::this_thread::sleep_for(std::chrono::microseconds(10));
