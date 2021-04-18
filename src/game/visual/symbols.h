557567ee63b3ffbf88ea2dbb28233625f3b0ba1f
julian-fuchs <julian7fuchs@gmail.com>
2021-04-18 15:41:19 +0200

    Remove unnecessary symbols

--- src/game/visual/symbols.h
+++ src/game/visual/symbols.h
@@ -17,8 +17,6 @@ const wchar_t box_drawings_light_down_and_left = L'\u2510';
 const wchar_t box_drawings_light_up_and_right = L'\u2514';
 const wchar_t box_drawings_light_up_and_left = L'\u2518';
 const wchar_t box_drawings_light_vertical_and_horizontal = L'\u253C';
-const wchar_t box_drawings_light_left = L'\u2574';
-const wchar_t box_drawings_light_right = L'\u2576';
 
 const wchar_t box_drawings_heavy_horizontal = L'\u2501';
 const wchar_t box_drawings_heavy_vertical = L'\u2503';
@@ -36,7 +34,6 @@ const wchar_t full_block = L'\u2588';
 const wchar_t light_shade = L'\u2591';
 const wchar_t medium_shade = L'\u2592';
 const wchar_t dark_shade = L'\u2593';
-const wchar_t white_circle = L'\u25CB';
 
 const wchar_t black_heart_suit = L'\u2665';  // rendered as an extra wide character
 const wchar_t dagger = L'\u2020';
